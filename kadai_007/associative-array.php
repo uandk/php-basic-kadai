0486051e7b6ae0fac23cecb27168b3ca075defd3
uandk <141488893+uandk@users.noreply.github.com>
2023-12-10 17:30:00 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>連想配列課題</title>
+</head>
+<body>
+<?php
+  $goods_details=['name'=>'onion','price'=>200,'weight'=>160];
+  print_r($goods_details);
+?>
+</body>
+</html>
